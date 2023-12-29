0d5ff113479b8d6a16e537cbb40d65ee4f7703b9
Dispact <tlsdbswp97@naver.com>
2023-12-29 16:59:49 +0900

    do nothing
    아무것도 하지 않은날

--- YunGine/DXRenderer/FbxLoader.cpp
+++ YunGine/DXRenderer/FbxLoader.cpp
@@ -16,17 +16,29 @@ bool FbxLoader::DoTheImortThing(const std::string& pFile)
 
 	const aiScene* scene = importer.ReadFile(
 		pFile,
-		aiProcess_CalcTangentSpace		|
-		aiProcess_Triangulate			|
+		aiProcess_CalcTangentSpace |
+		aiProcess_Triangulate |
 		aiProcess_JoinIdenticalVertices |
 		aiProcess_SortByPType);
 
-	if (scene == nullptr)
+	if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE)
 	{
 		return false;
 	}
 
-	return true;
+	scene->mMeshes;
+	for (unsigned int i = 0; i < scene->mNumMeshes; ++i)
+	{
+		auto mesh = scene->mMeshes[i];
+		if (mesh->HasFaces())
+		{
+			for (unsigned int i = 0; i < mesh->mNumVertices; ++i)
+			{
+				DirectX::XMFLOAT3 vertex = { mesh->mVertices->x,mesh->mVertices->y,mesh->mVertices->z };
+			}
+		}
 
+	}
+		return true;
 }
 
