a0bcc19da7d14df3845b4cf351728c8721a371ac
DESKTOP-7F78D4D\신윤제 <tlsdbswp97@naver.com>
2024-02-13 20:38:14 +0900

    Structural change in home
    쉐이더 파일을 약간 고쳤다

--- YunGine/DXRenderer/VertexShader.hlsl
+++ YunGine/DXRenderer/VertexShader.hlsl
@@ -45,6 +45,9 @@ PixelInputType main(VertexInputType input)
 {
     PixelInputType output;
     output.position = float4(input.position, 1.0f);
+    output.position = mul(input.position, worldMatrix);
+    output.position = mul(output.position, viewMatrix);
+    output.position = mul(output.position, projectionMatrix);
     output.color = input.color;
     return output;
 }
